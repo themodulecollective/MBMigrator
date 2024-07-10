7ea9ac1d99fe3219ae7619e0456e007ea162dd42
Mike Campbell <exactmike@users.noreply.github.com>
2024-07-10 19:53:03 -0400

    property shenanigans

--- MBMigrator/functions/Update-MBMActiveDirectoryData.ps1
+++ MBMigrator/functions/Update-MBMActiveDirectoryData.ps1
@@ -79,6 +79,7 @@ function Update-MBMActiveDirectoryData
                 'EmployeeType'
                 'HomePhone'
                 'Pager'
+                'Mobile'
                 'FacsimileTelephoneNumber'
                 'TelephoneNumber'
                 'Street'
