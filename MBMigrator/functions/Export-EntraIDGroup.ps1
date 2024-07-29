c1605c58cc68ddd60abbd2113192a89245169045
Mike Campbell <exactmike@users.noreply.github.com>
2024-07-29 07:27:53 -0400

    export-entraIDGroup fix params for get-oggroup

--- MBMigrator/functions/Export-EntraIDGroup.ps1
+++ MBMigrator/functions/Export-EntraIDGroup.ps1
@@ -44,8 +44,8 @@
     $OutputFileName = $TenantDomain + '-EntraIDGroups' + 'AsOf' + $DateString
     $OutputFilePath = Join-Path -Path $OutputFolderPath -ChildPath $($OutputFileName + '.xml')
 
-    $Groups = Get-OGGroup -ExcludeProperty GroupTypes -Property $Properties | 
-        Select-Object -Property @(
+    $Groups = Get-OGGroup -Property $Properties | 
+        Select-Object -ExcludeProperty GroupTypes -Property @(
             $Properties;
             @{n='TenantDomain';e={$TenantDomain}}, @{n='TenantID'; e={$TenantID}}, 
             @{n='groupType';e={$_.groupTypes -join '|'}})
