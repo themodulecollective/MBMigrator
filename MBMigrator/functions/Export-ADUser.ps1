e5f5ca0e4b9d54b2b5b95574ea3861477cda9e3c
Mike Campbell <exactmike@users.noreply.github.com>
2024-08-22 20:52:46 -0400

    more

--- MBMigrator/functions/Export-ADUser.ps1
+++ MBMigrator/functions/Export-ADUser.ps1
@@ -61,7 +61,7 @@
     $OutputFilePath = Join-Path -Path $OutputFolderPath -ChildPath $($OutputFileName + '.xml')
 
     $gADUserParams = @{}
-    $gADUserParams.Add('Server',$($Domain + ":3268"))
+    $gADUserParams.Add('Server',$($Domain))
 
     $ADUsers = Get-ADUser -Properties $Properties -filter * @gADUserParams  #| Sort-Object -Property $Properties -Descending
 
